3c22a69100d87bd42036feca0fceb46bc7e53cd8
Zhaojia.qiu <zhaojia.qiu@coolkit.cn>
2021-05-12 10:52:20 +0800

    chore: 自动同步文件

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,8 +1,12 @@
-// const appId = 'KOBxGJna5qkk3JLXw3LHLX3wSNiPjAVi';
-// const appSecret = '4v0sv6X5IM2ASIBiNDj6kGmSfxo40w7n';
+import { debugMode } from './config';
 
 // prod
-const appId = '4s1FXKC9FaGfoqXhmXSJneb3qcm1gOak';
-const appSecret = 'oKvCM06gvwkRbfetd6qWRrbC3rFrbIpV';
+let appId = '4s1FXKC9FaGfoqXhmXSJneb3qcm1gOak';
+let appSecret = 'oKvCM06gvwkRbfetd6qWRrbC3rFrbIpV';
+
+if (debugMode) {
+    appId = 'KOBxGJna5qkk3JLXw3LHLX3wSNiPjAVi';
+    appSecret = '4v0sv6X5IM2ASIBiNDj6kGmSfxo40w7n';
+}
 
 export { appId, appSecret };
